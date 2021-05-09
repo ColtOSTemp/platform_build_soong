f1d90cdaca31d5d382c57a76c0cdf86328ba55cb
Mrinal Ghosh <mg712702@gmail.com>
2021-05-09 14:02:48 +0530

    soong: Cleanup
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -139,7 +139,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 var BannerVars = []string{
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
-	"LINEAGE_VERSION",
+	"COLT_VERSION",
 	"TARGET_PRODUCT",
 	"TARGET_BUILD_VARIANT",
 	"TARGET_BUILD_TYPE",
@@ -164,7 +164,6 @@ var BannerVars = []string{
 	"TARGET_BUILD_PDK",
 	"PDK_FUSION_PLATFORM_ZIP",
 	"PRODUCT_SOONG_NAMESPACES",
-	"WITH_SU",
 	"WITH_GMS",
 }
 
